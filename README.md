b68d6fd83ac653cd5f95be3c3a5abadab54dd62b
Matt <msheppard@gmail.com>
2021-02-28 15:25:01 -0500

    I LOOKED AT MY NOTES AND I DIDN'T LIKE THEM

---
+++ README.md
@@ -0,0 +1,2 @@
+# collect google books ratings
+I've got list of about 4k books. I want to know which ones are rated highest by google books.
---
+++ parse.py
@@ -0,0 +1,64 @@
+from googleapiclient.discovery import build
+import time
+service = build('books', 'v1')
+sheets_service = build('sheets', 'v4')
+
+request = sheets_service.spreadsheets().get(spreadsheetId='15viB2N19RnIV0QMV2Pwb-0TV7f5luu0c81PHUuaHB7k')
+response = request.execute()
+sheet = response['sheets'][0]
+
+file = open('book_list', 'r')
+
+lines = file.readlines()
+
+def get_volume(name):
+    """gets single volume info from books api
+
+    Args:
+        name: name from directory name
+
+    Returns:
+        title(str) and rating(str)
+    """
+    request = service.volumes().list(q= name)
+    response = request.execute()
+
+    title = 'error'
+    rating = 0
+
+    try:
+        title = response['items'][0]['volumeInfo']['title']
+        rating = response['items'][0]['volumeInfo']['averageRating']
+    except:
+        pass
+
+    return title, rating
+
+values = []
+line_number = 1
+
+for line in lines:
+    # might need skipping logic in here
+
+    stripped = line.strip()
+    name_only = stripped.split('.')[0]
+    title, rating = get_volume(name_only)
+    print(name_only, title, rating)
+    
+    values.append([stripped, title, rating])
+
+    line_number += 1
+    range = f'A{line_number}'
+
+    if not (line_number % 10):
+        body = {'values': values}
+        result = sheets_service.spreadsheets().values().update(
+            spreadsheetId = '15viB2N19RnIV0QMV2Pwb-0TV7f5luu0c81PHUuaHB7k', 
+            body=body, range= range, valueInputOption='RAW').execute()
+        values = []
+    
+    # avoid quotas
+    time.sleep(5)
+
+service.close()
+sheets_service.close()
